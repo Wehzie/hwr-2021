41057a09c44ea7f47563ee596addbf2edafd530f
markrobertvandam <markrobertvandam@gmail.com>
2021-05-14 15:53:58 +0200

    Confusion matrix added

--- src/recognizer/train_test.py
+++ src/recognizer/train_test.py
@@ -1,5 +1,6 @@
 import os, sys, inspect, cv2
 import numpy as np
+import pandas as pd
 current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 project_root_dir = os.path.dirname(os.path.dirname(current_dir))
 sys.path.insert(0, project_root_dir)
@@ -35,29 +36,31 @@ class train_test():
                 self.dataset_builder.split_data()
         data_sets = os.listdir(read_path)
         X_train, y_train, X_dev, y_dev, X_test, y_test = [],[],[],[],[],[]
-        
+        img_size = (self.img_size[1], self.img_size[0])
+
         for letter in self.letters: 
             train_images = imagePaths = glob(f'{Path(read_path/"train"/letter)}/*.pgm')
             dev_images = imagePaths = glob(f'{Path(read_path/"dev"/letter)}/*.pgm')
             test_images = imagePaths = glob(f'{Path(read_path/"test"/letter)}/*.pgm')
+
             # training data
             for img in train_images:
                 image = cv2.imread(img)
-                image = cv2.resize(image, self.img_size)
+                image = cv2.resize(image, img_size)
                 X_train.append(image)
                 y_train.append(self.letters.index(letter))
 
             # dev data
             for img in dev_images:
                 image = cv2.imread(img)
-                image = cv2.resize(image, self.img_size)
+                image = cv2.resize(image, img_size)
                 X_dev.append(image)
                 y_dev.append(self.letters.index(letter))
 
             # test data
             for img in test_images:
                 image = cv2.imread(img)
-                image = cv2.resize(image, self.img_size)
+                image = cv2.resize(image, img_size)
                 X_test.append(image)
                 y_test.append(self.letters.index(letter))
 
@@ -70,6 +73,11 @@ class train_test():
         Char_Recognizer.model.compile(optimizer=keras.optimizers.Adam() , loss=keras.losses.SparseCategoricalCrossentropy(), metrics=['accuracy'])
         print(Char_Recognizer.get_summary())
         Char_Recognizer.model.fit(self.X_train, self.y_train, validation_data=(self.X_dev,self.y_dev), epochs=5)
+        
+        # Confusion matrix on dev data with final model
+        y_pred = Char_Recognizer.predict(self.X_dev)
+        y_predict = np.argmax(y_pred, axis = 1)
+        print(pd.crosstab(pd.Series(self.y_dev), pd.Series(y_predict), rownames = ["True:"], colnames = ["Predicted:"], margins = True))
 
     def test_model(self):
         Char_Recognizer = RecognizerModel()
