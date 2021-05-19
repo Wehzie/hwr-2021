d189bd4da720eba84bf8bd91f797bc3387a2bf11
Andele Swierstra <andele.rien@gmail.com>
2021-05-19 16:30:02 +0200

    Added roughly working hough transformation rotation correction

--- src/segmentor/line_extractor.py
+++ src/segmentor/line_extractor.py
@@ -1,7 +1,10 @@
-# %% 
-import cv2
+# %%
+import glob
+import os
+import cv2 as cv
 import numpy as np
-from scipy.signal import argrelmin, find_peaks
+from scipy import ndimage
+from scipy.signal import find_peaks
 
 
 class BoundingBox:
@@ -11,76 +14,148 @@ class BoundingBox:
         self.w = w
         self.h = h
 
+
 def get_bounding_boxes(img, max_height=250, min_area=10000):
-    ''' Takes a thresholded image and returns an array of bounding boxes. 
-        Bounding boxes with height smaller than max_height are split, boxes 
-        with area smaller than min_area are discarded.
-    '''
-    proj = np.sum(img,1)
-    peaks = find_peaks(proj, prominence=np.max(proj)/8)[0]
-    
+    """
+    Takes a thresholded image and returns an array of bounding boxes.
+    Bounding boxes with height smaller than max_height are split,
+    boxes with area smaller than min_area are discarded.
+    """
+    proj = np.sum(img, 1)
+    peaks = find_peaks(proj, prominence=np.max(proj) / 8)[0]
+
     # Add cross-through lines for text line center of gravity
     for y in peaks:
-        cv2.line(img, (0, y), (img.shape[1], y), 255, 30)  
+        cv.line(img, (0, y), (img.shape[1], y), 255, 30)
 
     # Estimate connected components
-    (numLabels, labels, stats, centroids) = cv2.connectedComponentsWithStats(img, 8, cv2.CV_32S)
-     
+    (num_labels, labels, stats, centroids) = cv.connectedComponentsWithStats(
+        img, 8, cv.CV_32S
+    )
+
     boxes = []
-    for i in range(1, numLabels):
-        x = stats[i, cv2.CC_STAT_LEFT]
-        y = stats[i, cv2.CC_STAT_TOP]
-        w = stats[i, cv2.CC_STAT_WIDTH]
-        h = stats[i, cv2.CC_STAT_HEIGHT]
-        area = stats[i, cv2.CC_STAT_AREA]
+    for i in range(1, num_labels):
+        x = stats[i, cv.CC_STAT_LEFT]
+        y = stats[i, cv.CC_STAT_TOP]
+        w = stats[i, cv.CC_STAT_WIDTH]
+        h = stats[i, cv.CC_STAT_HEIGHT]
+        area = stats[i, cv.CC_STAT_AREA]
         keep_height = h < max_height
         keep_area = area > min_area
         if keep_height and keep_area:
             boxes.append(BoundingBox(x, y, w, h))
         elif keep_area and not keep_height:
-            boxes += split_bounding_box(BoundingBox(x, y, w, h))   
-    return boxes 
+            boxes += split_bounding_box(BoundingBox(x, y, w, h))
+    return boxes
+
 
 def split_bounding_box(box):
-    ''' Just splits too large bounding box in the middle, might need more sophisticated method '''
-    box1 = BoundingBox(box.x, box.y, box.w, box.h//2)
-    box2 = BoundingBox(box.x, box.y+box.h//2, box.w, box.h//2)
-    return [box1, box2] 
+    """
+    Just splits too large bounding box in the middle,
+    might need more sophisticated method
+    """
+    box1 = BoundingBox(box.x, box.y, box.w, box.h // 2)
+    box2 = BoundingBox(box.x, box.y + box.h // 2, box.w, box.h // 2)
+    return [box1, box2]
+
 
 def clean_boxes(img, box, offset):
-    ''' Takes an image, bounding box and offset width as input and returns an image of the bounding box without
-    shapes introducing from other text lines'''
-    
+    """
+    Takes an image, bounding box and offset width as input and returns an image
+    of the bounding box without shapes introducing from other text lines
+    """
+
     img_pad = np.pad(img, ((offset, offset), (0, 0)))
-    sub_img = img_pad[box.y:(box.y+2*offset+box.h), (box.x):(box.x+box.w)]
-    
-    cv2.rectangle(sub_img,(0,0),(sub_img.shape[1], sub_img.shape[0]), 255, 1)
-    (numLabels, labels, stats, centroids) = cv2.connectedComponentsWithStats(sub_img, 8, cv2.CV_32S)
-    comp_id = np.argmax(stats[1:-1,cv2.CC_STAT_AREA])+1
-    
-    componentMask = (labels == comp_id).astype("uint8") * 255
-    res = np.where(componentMask, 0, sub_img)
+    sub_img = img_pad[box.y : (box.y + 2 * offset + box.h), (box.x) : (box.x + box.w)]
+
+    cv.rectangle(sub_img, (0, 0), (sub_img.shape[1], sub_img.shape[0]), 255, 1)
+    (num_labels, labels, stats, centroids) = cv.connectedComponentsWithStats(
+        sub_img, 8, cv.CV_32S
+    )
+    comp_id = np.argmax(stats[1:-1, cv.CC_STAT_AREA]) + 1
+
+    component_mask = (labels == comp_id).astype("uint8") * 255
+    res = np.where(component_mask, 0, sub_img)
     return res
 
-def extract_lines(filename):
-    '''Loads a given file from image-data and saves extracted lines
+
+def correct_rotation(image_path, threshold):
+    """
+    Rotate the image so the lines are (mostly) horizontal,
+    instead of (slightly) tilted.
+    TODO: Dynamically set threshold for better hough line detection in some
+     images while reducing unneccesarily high number of lines in others.
+    """
+    img = cv.imread(image_path, cv.IMREAD_GRAYSCALE)
+
+    if img is None:
+        print("Image not found")
+        return
+
+    edges = cv.Canny(img, 50, 200)
+    lines = cv.HoughLines(
+        edges, 1, np.pi / 180, threshold, min_theta=0.45 * np.pi, max_theta=0.55 * np.pi
+    )
+
+    if lines is None:
+        print("No lines found.")
+        return img
+
+    hough = img.copy()
+    for r, theta in lines[:, 0, :]:
+        a = np.cos(theta)
+        b = np.sin(theta)
+
+        x0 = a * r
+        y0 = b * r
+
+        x1 = int(x0 + 8000 * (-b))
+        y1 = int(y0 + 8000 * (a))
+        x2 = int(x0 - 8000 * (-b))
+        y2 = int(y0 - 8000 * (a))
+        cv.line(hough, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+    cv.imwrite(f"img/{os.path.basename(image_path)}_hough.png", hough)
+
+    thetas = lines[:, 0, 1] - 0.5 * np.pi
+    rotation = np.degrees(np.mean(thetas))
+    # Other rotation finding techniques that are currently not in use, using
+    # binning instead of simple averaging. Seems like it had a worse effect.
+    # bins = np.array([np.radians(deg) for deg in range(-20, 21)])
+    # binned = np.digitize(thetas, bins)
+    # rotation = np.degrees(bins[stats.mode(binned).mode[0]])
+    # rotation = np.degrees(np.mean(bins[np.array(Counter(binned).most_common(3))[:, 0]]))
+
+    print(f"Avg. rotation based on {lines[:, 0, 1].size} lines: {rotation} deg.")
+    return ndimage.rotate(img, rotation, cval=255)
+
+
+def extract_lines(image_path):
+    """
+    Loads a given file from image-data and saves extracted lines
     to the lines folder
-    '''
-    img = cv2.imread(f"image-data/{filename}", cv2.IMREAD_GRAYSCALE)
+    """
+
+    file_name = os.path.basename(image_path)
+    rotated_img = correct_rotation(image_path, 100)
+    cv.imwrite(f"img/{file_name}.png", rotated_img)
 
     # Threshold and invert
-    img = cv2.threshold(img,127,255,cv2.THRESH_BINARY)[1]
-    img = cv2.bitwise_not(img)
+    img = cv.threshold(rotated_img, 127, 255, cv.THRESH_BINARY)[1]
+    img = cv.bitwise_not(img)
     lines_img = img.copy()
-    
+
     boxes = get_bounding_boxes(lines_img)
+
     for i, box in enumerate(boxes):
-        clean = clean_boxes(img, box, 10)
-        cv2.imwrite(f"lines/{filename}_L{i}.png", cv2.bitwise_not(clean))
+        # clean = clean_boxes(img, box, 10)
+        clean = img[box.y : box.y + box.h, box.x : box.x + box.w]
+        cv.imwrite(f"lines/{file_name}_L{i}.png", cv.bitwise_not(clean))
 
 
 if __name__ == "__main__":
     # Change this to loop which iterates over all binary images in image_data
-    filename = 'P344-Fg001-R-C01-R01-binarized.jpg'
-    extract_lines(filename)
+    for img in glob.glob("../../data/image-data/*binarized.jpg"):
+        print(f"Processing {os.path.basename(img)}")
+        extract_lines(img)
 # %%
