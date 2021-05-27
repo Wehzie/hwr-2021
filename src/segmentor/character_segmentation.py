cdbbe6989574cdedaa845e3b6bf8417070e6fa8d
markrobertvandam <markrobertvandam@gmail.com>
2021-05-27 20:53:12 +0200

    Base of character segmentation

---
+++ src/segmentor/character_segmentation.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+
+import os
+import cv2 as cv
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+
+from dotenv import load_dotenv
+from glob import glob
+
+class Character_segmentor():
+    
+    load_dotenv()
+
+    def __init__(self) -> None:
+        pass
+
+
+    def place_holder(self):
+        img_folder = "lines/"
+        read_path = Path(os.environ["DATA_PATH"]) / img_folder
+        for image_path in glob(f"{read_path}/*.png"):
+            image = cv.imread(image_path)
+            print(image_path)
+            print(image.shape)
+            tmp = image
+            step_size = 40
+            window_size = (68,40)
+            for x in range(0, image.shape[1] - window_size[1], step_size):
+                window = image[x:x + window_size[1], 0: window_size[0], :]
+                cv.rectangle(tmp, (x, 0), (x + window_size[1], window_size[0]), (255, 0, 0), 2) # draw rectangle on image
+                plt.imshow(np.array(tmp).astype('uint8'))
+            plt.savefig('boxes.png')
+            break
+
+if __name__ == "__main__":
+    char_segmentor = Character_segmentor()
+    char_segmentor.place_holder()
