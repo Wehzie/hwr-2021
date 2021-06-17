cb2415d2ed7c00b77e773480d7a0cdf135579bb2
L-Cordes <l.cordes@student.rug.nl>
2021-06-17 16:32:39 +0200

    implemented dumb middle split

--- src/segmentor/split_conn_test.py
+++ src/segmentor/split_conn_test.py
@@ -24,51 +24,6 @@ class ArgParser(Tap):
         self.add_argument("output_dir")
 
 
-def get_bounding_boxes(img: np.ndarray, min_pixel=120) -> List[BoundingBox]:
-    """Get bounding boxes of characters in a word image.
-
-    img: word image
-    min_pixel: minimum amount of ink in a word
-    L: filter length
-    return: bounding boxes for each word in a line (from left to right)
-    """
-    img = img.copy()  # deep copy, not a reference
-    new_img = img.copy()
-
-    for i in range(len(new_img)):
-        for j in range(len(new_img[i])):
-            new_img[i][j] /= 255
-
-    new_img = skeletonize(new_img)
-
-    new_img = new_img.astype("uint8") * 255
-
-    proj = np.sum(new_img, 0)
-    proj = np.true_divide(proj, np.amax(proj))
-
-    # idea: a word is a series of non-zeroes
-    boxes = []
-    height, width = img.shape
-    box = BoundingBox(None, 0, None, height)
-    for i, val in enumerate(proj):
-        # character beginning
-        if val != 0.0 and box.x is None:
-            box.x = i  # set box x
-        # character end
-        if val == 0.0 and box.x is not None:
-            box.w = i - box.x
-            boxed_img = box.image_slice(img)
-            # minimum number of non-white pixes
-            if np.count_nonzero(boxed_img) > min_pixel:
-                boxes.append(box)  # add box to list
-            box = BoundingBox(None, 0, None, height)  # reset box
-    return boxes
-
-
-def split_non_connected(chars: List[np.ndarray]) -> List[np.ndarray]:
-    return chars
-
-
 def middle_split(char: np.ndarray) -> List[np.ndarray]:
     middle = char.shape[1] // 2
     return [char[:, :middle], char[:, middle:]]
@@ -88,28 +43,14 @@ def split_connected(chars: List[np.ndarray]) -> List[np.ndarray]:
     return split_chars
 
 
-def extract_characters(img: np.ndarray, read_ord="r2l") -> List[np.ndarray]:
+def extract_characters(char: np.ndarray, read_ord="r2l") -> List[np.ndarray]:
     """Load a word image and return extracted characters."""
 
     # Threshold and invert
     img = cv.threshold(img, 127, 255, cv.THRESH_BINARY)[1]
     img = cv.bitwise_not(img)
 
-    boxes = get_bounding_boxes(img)
-
-    if read_ord == "r2l":  # words in order right to left
-        boxes = reversed(boxes)
-
-    characters: List[np.ndarray] = []
-    for i, box in enumerate(boxes):
-        # clean = clean_boxes(img, box)
-        clean = box.image_slice(img)
-        inversed = cv.bitwise_not(clean)
-        if box.w > 16:
-            characters.append(inversed)
-
-    characters = split_non_connected(characters)
-    characters = split_connected(characters)
+    characters = split_connected(img)
 
         # cv.imwrite(
         #    str((out_dir / f"{path_stem}c{i}.png").resolve()),
@@ -122,6 +63,11 @@ if __name__ == "__main__":
     args = ArgParser(
         description="Extract characters from binarized dead sea scroll pictures"
     ).parse_args()
+
+
+
+    ### CHANGE: load only split images 
+
 
     # Extract lines
     for img_path in args.input_dir.glob("*binarized.jpg"):  # For each fragment
